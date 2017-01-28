17b39152a3cde451f084357c9969d9151f6d4f10
James Wesley Goedert <jwgoedert@gmail.com>
2017-01-28 14:38:54 -0600

    random
    uses Math and Math.floor to search out a random number from an array of numbers

---
+++ random.js
@@ -0,0 +1,8 @@
+//Random
+
+var random = function(collection){
+    return Math.floor(Math.random(collection) * collection.length);
+}
+console.log(random([1,2,3,4,5,6,7,7]));
+
+///////////////////////////////////////////////////
